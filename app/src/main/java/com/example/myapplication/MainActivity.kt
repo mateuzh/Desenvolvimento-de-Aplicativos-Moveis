57ec29b1bb2d888b5f3e1a2fe8ecf6553e7b47cf
App <mateushh213@gmail.com>
2023-08-23 20:17:53 -0300

    Start commit!

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.ui.login.LoginFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tabLayoutObject: TabLayout = findViewById(R.id.tabLayout)
+        tabLayoutObject.addOnTabSelectedListener(
+            object: OnTabSelectedListener{
+                override fun onTabSelected(tab: Tab?) {
+                    when (tab?.position){
+                        0 -> supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.fragmentContainerView, LoginFragment())
+                            .commit()
+                        1 -> supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.fragmentContainerView, SettingsFragment())
+                            .commit()
+                        2 -> supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.fragmentContainerView, BlankFragment())
+                            .commit()
+                    }
+                }
+
+                override fun onTabUnselected(tab: Tab?) {
+
+                }
+
+                override fun onTabReselected(tab: Tab?) {
+
+                }
+            }
+        )
     }
 }
